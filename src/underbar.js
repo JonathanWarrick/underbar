5bf8a906d0d57e99f0949422de7adc78377be625
JonathanWarrick <jonathan.warrick@gmail.com>
2014-06-19 17:59:34 -0400

    Added functionality for _.delay.  Passes all tests.

--- src/underbar.js
+++ src/underbar.js
@@ -354,6 +354,7 @@ var _ = {};
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+
   };
 
   // Delays a function for the given number of milliseconds, and then calls
@@ -363,6 +364,11 @@ var _ = {};
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var funcArgs = Array.prototype.slice.call(arguments, 2);
+
+    return setTimeout(function() {
+      func.apply(this, funcArgs)
+    }, wait);
   };
 
 
